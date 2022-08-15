dcbb97ce760a9413e72ed3324ba698df99e15a19
tonisimanjuntak <tonibackupfile@gmail.com>
2022-08-15 17:47:57 +0700

    add track visitor

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/userguide3/general/hooks.html
 |
 */
+
+/*
+  | -------------------------------------------------------------------------
+  | Track Visitor
+  | -------------------------------------------------------------------------
+  | log visitor info
+  |
+ */
+$hook['post_controller_constructor'][] = array(
+    'class' => 'Track_Visitor',
+    'function' => 'visitor_track',
+    'filename' => 'Track_Visitor.php',
+    'filepath' => 'hooks'
+);
