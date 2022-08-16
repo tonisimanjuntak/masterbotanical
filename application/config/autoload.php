6456224a97ffcc54f3e61d906142def432f32030
tonisimanjuntak <tonibackupfile@gmail.com>
2022-08-16 13:20:19 +0700

    rename site_config

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'encrypt', 'email', 'cart', 'site_config');
+$autoload['libraries'] = array('database', 'session', 'encrypt', 'email', 'cart', 'Site_Config');
 
 /*
 | -------------------------------------------------------------------
